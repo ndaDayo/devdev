379976c841cbac724eede601ac10776a65118c78
ndaDayo <hayato711+nda@gmail.com>
2024-02-20 13:32:33 +0900

    add: new client

--- api/github.go
+++ api/github.go
@@ -1,6 +1,9 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 const (
 	BASEURL = "https://api.github.com/"
@@ -14,4 +17,16 @@ type Client struct {
 
 type httpClient interface {
 	Do(*http.Request) (*http.Response, error)
+}
+
+func NewClient(token, endpoint string) *Client {
+	c := &Client{
+		token:    token,
+		endpoint: endpoint,
+		httpClient: &http.Client{
+			Timeout: 60 * time.Second,
+		},
+	}
+
+	return c
 }
