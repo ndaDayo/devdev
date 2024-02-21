6583cd3f23eca64a21160cd42541dff2b2a884fa
ndaDayo <hayato711+nda@gmail.com>
2024-02-21 22:25:32 +0900

    mod: paramの受け渡し

--- api/commit.go
+++ api/commit.go
@@ -93,8 +93,14 @@ type Verification struct {
 
 type CommitService service
 
-func (s *CommitService) Get(ctx context.Context, owner, repo, ref string) (*Commit, *Response, error) {
-	path := fmt.Sprintf("/repos/%v/%v/commits/%v", owner, repo, ref)
+type CommitParam struct {
+	Owner string
+	Repo  string
+	Ref   string
+}
+
+func (s *CommitService) Get(ctx context.Context, p CommitParam) (*Commit, *Response, error) {
+	path := fmt.Sprintf("/repos/%v/%v/commits/%v", p.Owner, p.Repo, p.Ref)
 	req, err := s.client.NewRequest("GET", path)
 	if err != nil {
 		return nil, nil, err
