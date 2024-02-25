15d8385a193106bff33c2151474ee1c36d835f88
ndaDayo <hayato711+nda@gmail.com>
2024-02-25 15:50:38 +0900

    wip: github_activities

--- usecase/activity/github_activities.go
+++ usecase/activity/github_activities.go
@@ -1,7 +1,41 @@
 package activity_uc
 
-import entity "github.com/ndaDayo/devdev/entity/activity"
+import (
+	"context"
+	"errors"
+	"fmt"
 
-func githubActivities(p ActivityPeriod) (*entity.Github, error) {
-	return &entity.Github{}, nil
+	"github.com/ndaDayo/devdev/api"
+	entity "github.com/ndaDayo/devdev/entity/activity"
+)
+
+type GithubParams struct {
+	Username string
+	Repo     string
+}
+
+func (g GithubFetcher) FetchActivity(params interface{}) (*entity.Activity, error) {
+	gp, ok := params.(*GithubParams)
+	if !ok {
+		return nil, errors.New("invalid parameters for GitHub fetcher")
+	}
+
+	client := api.NewClient(api.WithToken())
+	ctx := context.Background()
+
+	prm := api.CommitsParam{
+		Owner: gp.Username,
+		Repo:  gp.Repo,
+	}
+
+	commits, _, err := client.Commits.Get(ctx, prm)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	for _, c := range *commits {
+		fmt.Println(c.Commit.Message)
+	}
+
+	return &entity.Activity{}, nil
 }
