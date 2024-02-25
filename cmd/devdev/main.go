7fb8db2f31805975e6d89c1e57fdab081c410fdc
ndaDayo <hayato711+nda@gmail.com>
2024-02-25 22:31:44 +0900

    add: activity cmd

--- cmd/devdev/main.go
+++ cmd/devdev/main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/ndaDayo/devdev/cmd/cmd"
+import "github.com/ndaDayo/devdev/cmd/devdev/cmd"
 
 func main() {
 	cmd.Execute()
