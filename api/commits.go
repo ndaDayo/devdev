5a89377bfde4d9af97c70a2b192a6559028977d7
ndaDayo <hayato711+nda@gmail.com>
2024-02-27 17:07:53 +0900

    add: query

--- api/commits.go
+++ api/commits.go
@@ -32,8 +32,24 @@ type user struct {
 type CommitsService service
 
 type CommitsParam struct {
+	path  path
+	query query
+}
+
+type path struct {
 	Owner string
 	Repo  string
+}
+
+type query struct {
+	Sha       string
+	Path      string
+	Author    string
+	Committer string
+	Since     string
+	Until     string
+	PerPage   int
+	Page      int
 }
 
 func (s *CommitsService) Get(ctx context.Context, p CommitsParam) (*Commits, *Response, error) {
