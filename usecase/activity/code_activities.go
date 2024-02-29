a9a587d4e896a48bb37eeee2d15c39e1a6ed2fc5
ndaDayo <hayato711+nda@gmail.com>
2024-02-29 21:17:01 +0900

    mod: div function

--- usecase/activity/code_activities.go
+++ usecase/activity/code_activities.go
@@ -14,34 +14,29 @@ type CodeParams struct {
 	query    api.Query
 }
 
+type CodeActivity struct {
+	Owner string
+	Repo  string
+}
+
+func NewCodeActivity(owner, repo string) *CodeActivity {
+	return &CodeActivity{Owner: owner, Repo: repo}
+}
+
 func (c CodeActivityFetcher) FetchActivity(params interface{}) (*entity.Activity, error) {
 	cp, ok := params.(CodeParams)
 	if !ok {
 		return nil, errors.New("invalid params type")
 	}
 
-	cm, err := getCommits(cp)
+	ca := NewCodeActivity(cp.Username, cp.Repo)
+	cm, err := ca.getCommits()
 	if err != nil {
 		fmt.Println("Error fetching commits:", err)
 		return nil, err
 	}
 
 	for _, c := range *cm {
-		prm := api.CommitParam{
-			Owner: cp.Username,
-			Repo:  cp.Repo,
-			Ref:   c.SHA,
-		}
-
-		commit, err := api.GetResource(prm)
-		if err != nil {
-			fmt.Println("err", err)
-		}
-
-		cm, ok := commit.(*api.Commit)
-		if !ok {
-			return nil, errors.New("")
-		}
 	}
 
 	activity := entity.NewActivity()
@@ -49,11 +44,11 @@ func (c CodeActivityFetcher) FetchActivity(params interface{}) (*entity.Activity
 	return activity, nil
 }
 
-func getCommits(cp CodeParams) (*api.Commits, error) {
+func (ca *CodeActivity) getCommits() (*api.Commits, error) {
 	prm := api.CommitsParam{
 		Path: api.Path{
-			Owner: cp.Username,
-			Repo:  cp.Repo,
+			Owner: ca.Owner,
+			Repo:  ca.Repo,
 		},
 		Query: api.Query{
 			Since: "",
@@ -73,4 +68,24 @@ func getCommits(cp CodeParams) (*api.Commits, error) {
 
 	return cm, nil
 
+}
+
+func (ca *CodeActivity) getCommit(ref string) (*api.Commit, error) {
+	prm := api.CommitParam{
+		Owner: ca.Owner,
+		Repo:  ca.Repo,
+		Ref:   ref,
+	}
+
+	commit, err := api.GetResource(prm)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	cm, ok := commit.(*api.Commit)
+	if !ok {
+		return nil, errors.New("")
+	}
+
+	return cm, nil
 }
