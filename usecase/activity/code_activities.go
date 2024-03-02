9843d130bc569c0638a428ea6b69b170a4540070
ndaDayo <hayato711+nda@gmail.com>
2024-03-02 16:00:58 +0900

    mod: code params

--- usecase/activity/code_activities.go
+++ usecase/activity/code_activities.go
@@ -4,14 +4,13 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/ndaDayo/devdev/api"
 	entity "github.com/ndaDayo/devdev/entity/activity"
 )
 
 type CodeParams struct {
-	Username string
+	Owner    string
 	Repo     string
-	query    api.Query
+	Username string
 }
 
 func (c CodeActivityFetcher) FetchActivity(params interface{}) (*entity.Activity, error) {
@@ -37,50 +36,4 @@ func (c CodeActivityFetcher) FetchActivity(params interface{}) (*entity.Activity
 	activity := entity.NewActivity()
 
 	return activity, nil
-}
-
-func (ca *CodeActivity) getCommits() (*api.Commits, error) {
-	prm := api.CommitsParam{
-		Path: api.Path{
-			Owner: ca.Owner,
-			Repo:  ca.Repo,
-		},
-		Query: api.Query{
-			Since: "",
-			Until: "",
-		},
-	}
-
-	commits, err := api.GetResource(prm)
-	if err != nil {
-		fmt.Println("err", err)
-	}
-
-	cm, ok := commits.(*api.Commits)
-	if !ok {
-		return nil, errors.New("")
-	}
-
-	return cm, nil
-
-}
-
-func (ca *CodeActivity) getCommit(ref string) (*api.Commit, error) {
-	prm := api.CommitParam{
-		Owner: ca.Owner,
-		Repo:  ca.Repo,
-		Ref:   ref,
-	}
-
-	commit, err := api.GetResource(prm)
-	if err != nil {
-		fmt.Println("err", err)
-	}
-
-	cm, ok := commit.(*api.Commit)
-	if !ok {
-		return nil, errors.New("")
-	}
-
-	return cm, nil
 }
