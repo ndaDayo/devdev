be6c1714c183b0fd20bc8f8ef76a9f5f481fcf4a
ndaDayo <hayato711+nda@gmail.com>
2024-03-15 14:07:02 +0900

    add: logger interface

--- adapter/api/logging/info.go
+++ adapter/api/logging/info.go
@@ -1,5 +1,7 @@
 package logging
 
+import "github.com/ndaDayo/devdev/adapter/logger"
+
 type Info struct {
 	log        logger.Logger
 	resource   string
@@ -7,9 +9,9 @@ type Info struct {
 	httpStatus int
 }
 
-func NewInfo(log logger.Logger, resource, context string, httpStatus int) {
+func NewInfo(log logger.Logger, resource, context string, httpStatus int) Info {
 	return Info{
-		log:        logger.Logger,
+		log:        log,
 		resource:   resource,
 		context:    context,
 		httpStatus: httpStatus,
